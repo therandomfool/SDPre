d2944c1df17baf73e837b8fda01131b37a688c55
therandomfool <MDdenver@protonmail.com>
2020-02-29 01:06:20 -0700

    JTHW ex 8 Storing the Human Response Study Q's 1-4

--- JavaTheHardWay/RudeQuestions.java
+++ JavaTheHardWay/RudeQuestions.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class RudeQuestions {
   public static void main(String[] args) {
-    String name;
+    String name, pet;
     int age;
     double weight, income;
 
@@ -10,6 +10,10 @@ public class RudeQuestions {
 
     System.out.println( "Hello. What is your name? " );
     name = keyboard.next();
+
+    System.out.println( name + " what kind of pet do you have?");
+    pet = keyboard.next();
+    System.out.println( name + " you seem like you would have a " + pet );
 
     System.out.println( "Hi, " + name + "! How old are you? " );
     age = keyboard.nextInt();
@@ -26,5 +30,11 @@ public class RudeQuestions {
     System.out.println( " and not per year!");
     System.out.println( "Well, thanks for answering my rude questions, ");
     System.out.println( name + ".");
+
+    // integer does not blow up double.  just represents 1 as 1.0  2 @ 2.0 etc
+    // integer or double does not blow up when input as String.  Variable is just a String, cannot do math on import junit.framework.TestCase;
+    // q1 accepts everything.
+    // q2 blows up on String and double needing just int
+    // q3 & q4 blows up with String needing numerical input
   }
 }
