db0711cb8b363f993c61b70c5023064743f95003
therandomfool <MDdenver@protonmail.com>
2020-02-28 22:17:47 -0700

    JTHW Objective 2 Lab 5 Right Triangle

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,10 @@
+public class Objective2Lab5 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+
+    side1 = 10;
+    side2 = 8;
+
+    System.out.println("The hypotenuse of a triangle with sides " + side1 + "and " + side2 + " is " + Math.hypot(side1, side2));
+  }
+}
