8cf9b0bc02a4d5306f7dc16d5dcf795851c331cd
therandomfool <MDdenver@protonmail.com>
2020-02-28 19:47:25 -0700

    JTHW Lab3 Creating a class

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+    System.out.println("Name : MD");
+    System.out.println("Age : 12");
+    System.out.println("State : Colorado");
+  }
+}
