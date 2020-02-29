77da702495e9b385424ec09bc3db82c36ebf1e57
therandomfool <MDdenver@protonmail.com>
2020-02-28 21:54:21 -0700

    JTHW Objective 2 Lab 4 Writing Expressions

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,13 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 2897;
+    num2 = 318;
+    num3 = 782409;
+
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println(average);
+  }
+}
