2308d351e1fcf6a7d50435945a0645f5fdc69c8e
therandomfool <MDdenver@protonmail.com>
2020-02-28 21:27:50 -0700

    JTHW Objective 2 Lab 2 Printing Multiple Variables

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "MD";
+    int age = 12;
+    String state = "Colorado";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state );
+  }
+}
