36d3b1cdbdcac0129d8d6e3c02105f8298f58aa4
therandomfool <MDdenver@protonmail.com>
2020-03-01 13:55:24 -0700

    JTHW Exercise 17 Otherwise (If Statements with Else)  Study Drill Q#1

--- JavaTheHardWay/ClubBouncer.java
+++ JavaTheHardWay/ClubBouncer.java
@@ -8,8 +8,11 @@ public class ClubBouncer {
     if ( onGuestList || age >= 21 || (gender.equals("F") && allure >=8)) {
       System.out.println( "You are allowed to enter the club." );
     }
+    // System.out.println( " C-C-C-COMBO BREAKER");
     else {
       System.out.println( "You are not allowed to enter the club." );
     }
+
+    // Study Drill Q#1 adding line in between if and else statements breaks it.  if and else need to be in same body of code
   }
 }
