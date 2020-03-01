8dfe30871fc21a9654a8e41b249f2ee6028df82c
therandomfool <MDdenver@protonmail.com>
2020-02-29 18:54:59 -0700

    Exercise 14 Compound Boolean Expressions Study Drill Q#1

--- JavaTheHardWay/ShallowGrandmother.java
+++ JavaTheHardWay/ShallowGrandmother.java
@@ -4,8 +4,8 @@ public class ShallowGrandmother {
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
     int age;
-    double income, cute;
-    boolean allowed;
+    double income, cute, happy;
+    boolean allowed, allowed2;
 
     System.out.println( "Enter your age: " );
     age = keyboard.nextInt();
@@ -19,5 +19,16 @@ public class ShallowGrandmother {
     allowed = ( age > 25 &&  age < 40 && ( income > 50000 || cute >= 8.5 ) );
 
     System.out.println( "Allowed to date my grandchild? " + allowed );
+
+    // Second Grandma
+    System.out.println( "How happy do you make them on a scale of 0.0 to 10.0? " );
+    happy = keyboard.nextDouble();
+
+    allowed2 = ( age > 30 && age < 45 && happy >= 7 );
+
+    System.out.println( "<second Granny>Allowed to date my grandchild? " + allowed );
+
+
+
   }
 }
