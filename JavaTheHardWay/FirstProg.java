208820a9accc439a45c83d0f66bf89d725c4daef
therandomfool <MDdenver@protonmail.com>
2020-02-29 19:00:46 -0700

    FirstProg

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,8 +1,6 @@
 public class FirstProg{
-  
   public static void main(String[] args) {
-    System.out.println("I am determined that you will learn code");
-    System.out.println("Today is a great day");
+    System.out.println("MD is determined to learn how to code");
+    System.out.println("Today's date is 2.28.2020");
   }
-
 }
