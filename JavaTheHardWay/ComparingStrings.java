cccbfb2ebf516415f85e5894b3d2befff3e3f4e4
therandomfool <MDdenver@protonmail.com>
2020-02-29 17:59:52 -0700

    JTHW Exercise 13 Comparing Strings Study Drill #1

--- JavaTheHardWay/ComparingStrings.java
+++ JavaTheHardWay/ComparingStrings.java
@@ -9,10 +9,12 @@ import java.util.Scanner;
      System.out.println( "Type the word \"weasel\", please." );
      word = keyboard.next();
 
-     yep = word.equals("weasel");
+     yep = "weasel".equals(word);
      nope = ! word.equals("weasel");
 
      System.out.println( "You typed what was requested: " + yep );
      System.out.println( "You ignored polite instructions: " + nope );
+
+     //  Study Drill switching "weasel" and word around worked fine.  Made the same equation
    }
  }
