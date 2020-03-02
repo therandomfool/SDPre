0ba96731b06ab20bfd1939f7e99c0df7294bbfec
therandomfool <MDdenver@protonmail.com>
2020-03-02 00:08:45 -0700

    JTHW Objective5Lab1 Month Names

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,19 @@
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+    int month = 6;
+
+    switch(month) {
+      case 1:
+        System.out.println( "January");
+        break;
+
+      case 6:
+      System.out.println( "June");
+      break;
+
+      default:
+      System.out.println( "That is not a valid month" );
+      break;
+    }
+  }
+}
