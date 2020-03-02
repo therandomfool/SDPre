69a51eeefbf5b924e38d2dc13b806607d7f910c1
therandomfool <MDdenver@protonmail.com>
2020-03-01 22:09:02 -0700

    JTHW Objective4Lab1 Find Max Value

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,17 @@
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+
+
+    if (num1 > num2) {
+      System.out.println( num1 + " is bigger than " + num2);
+    }
+    else if (num2 > num1) {
+      System.out.println( num2 + " is bigger than " + num1);
+    }
+
+    // need to add operator for instances the numbers are equal
+  }
+}
