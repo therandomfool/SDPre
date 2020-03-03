61c4dc8e88461e9a671f8f5719871414f5ea10d0
therandomfool <MDdenver@protonmail.com>
2020-03-02 18:59:33 -0700

    JTHW Excercise 25 Repeating Yourself with the While Loop Study Drills Q#1

--- JavaTheHardWay/EnterPIN.java
+++ JavaTheHardWay/EnterPIN.java
@@ -4,10 +4,22 @@ public class EnterPIN {
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
     int pin, entry;
+    String pass, word;
 
+    word = "word";
     pin = 12345;
 
     System.out.println( "WELCOME TO THE BANK OF JAVA." );
+    System.out.println( "ENTER YOUR PASSWORD: ");
+    pass = keyboard.nextLine();
+
+    while ( ! pass.equals(word) ) {
+      System.out.println( "\n INCORRECT PASSWORD. TRY AGAIN. " );
+      System.out.println( "ENTER YOUR PASSWORD: ");
+      pass = keyboard.nextLine();
+    }
+
+    System.out.println( "\n PASSWORD ACCEPTED...");
     System.out.println( "ENTER YOUR PIN: ");
     entry = keyboard.nextInt();
 
