ac1d5e756140c1265ac3351dfbaa94f8c79e14cd
therandomfool <MDdenver@protonmail.com>
2020-03-03 11:05:59 -0700

    JTHW Exercise 30 adding Values One at a Time Study Drills Q#1 & 2

--- JavaTheHardWay/RunningTotal.java
+++ JavaTheHardWay/RunningTotal.java
@@ -12,9 +12,39 @@ public class RunningTotal {
       System.out.println("Value: " );
       current = keyboard.nextInt();
       total += current;
-      System.out.println("The total so far is: " + total);
+      // Study Drills Q#2 add if statement to make sure current is not 0 to continue to total.  If its 0 then it moves to while statement
+      if ( current != 0 )
+        System.out.println("The total so far is: " + total);
     } while ( current != 0 );
 
     System.out.println( "The final total is : " + total );
   }
 }
+
+
+
+//  Study Drills Q#1
+
+// import java.util.Scanner;
+//
+// public class RunningTotal {
+//   public static void main(String[] args) {
+//     Scanner keyboard = new Scanner(System.in);
+//     int current, total = 0;
+//
+//     System.out.println( "Type in a bunch of values and I'll add them up. " );
+//     System.out.println( "I'll stop when you type a zero. " );
+//
+//     System.out.println("Value: " );
+//     current = keyboard.nextInt();
+//
+//     while ( current != 0 ) {
+//       System.out.println("Value: " );
+//       current = keyboard.nextInt();
+//       total += current;
+//       System.out.println("The total so far is: " + total);
+//     }
+//
+//     System.out.println( "The final total is : " + total );
+//   }
+// }
