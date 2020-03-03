7e19178d438ca2d0c5a5cc5b76d03e07282cbf0d
therandomfool <MDdenver@protonmail.com>
2020-03-02 21:40:55 -0700

    Exercise 28 Using Loops for Error-Checking Study Drills Q#1

--- JavaTheHardWay/SafeSquareRoot.java
+++ JavaTheHardWay/SafeSquareRoot.java
@@ -4,6 +4,15 @@ public class SafeSquareRoot {
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
     double x, y;
+    String z;
+
+    System.out.println( "Are you ready?  ");
+    z = keyboard.nextLine();
+
+    while ( ! z.equals("YES") ) {
+      System.out.println("I DON'T THINK YOU ARE READY! " );
+      z = keyboard.nextLine();
+    }
 
     System.out.println( "Give me a number, and I'll find out it's square root. " );
     System.out.println( "(No negatives, please.) ");
