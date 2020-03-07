e8c8569db576e8eb14ee6a199799a2f3207848bc
therandomfool <mddenver@protonmail.com>
2020-03-07 13:32:19 -0700

    JTHW Exercise 36 Returning a value from a Function Study Drill Q#1,2 &3

--- JavaTheHardWay/HeronsFormula.java
+++ JavaTheHardWay/HeronsFormula.java
@@ -14,14 +14,22 @@ public class HeronsFormula {
     System.out.println(tws + "5,12,13 has area " + triangleArea(5,12,13) );
     System.out.println(tws + "10,9,11 has area " + triangleArea(10,9,11) );
     System.out.println(tws + "8,15,17 has area " + triangleArea(8,15,17) );
+    System.out.println(tws + "9,9,9 has area " + triangleArea(9,9,9) );
+
   }
     // This function coputes area of triangle with sides a,b,c
   public static double triangleArea(int a, int b, int c) {
     double s, A;
 
-    s= (a+b+c) / 2;
+    s= (a+b+c) / 2.0;
     A = Math.sqrt( s*(s-a)*(s-b)*(s-c) );
 
     return A;
   }
+    // Study Drills
+   // Q#1 the code without the function is longer
+   // Q#2 changing 2 to 2.0 in a function requires one fix instead of multiple fixes on multiple lines
+   // Q#3 similare to Q#2 changing one function interchangable DRY code rather than changing multiple lines of code to achieve same results without a function
+   
+
 }
