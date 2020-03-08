adfe7918bddd5cb6eb6411bff6237e4d448d13d7
therandomfool <mddenver@protonmail.com>
2020-03-08 13:29:37 -0600

    Still needs to be finished

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    // Scanner keyboard = new Scanner(System.in);
+    // String name;
+    //
+    // System.out.println("What is your name? " );
+    // name = keyboard.next();
+    // System.out.println("Hello " + printName(name));
+  }
+  public static void printName(){
+    // System.out.println("Hello " + printName(name));
+
+  }
+}
